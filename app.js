f02d9c698dcc06bff62ee6cd53e13312cb466fff
Zishan Ahmed Onik <zisanik@gmail.com>
2023-01-14 22:27:25 -0600

    possibly fixed the large file size error

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const busboy = require("connect-busboy");
 
 var indexRouter = require("./routes/index");
 const seasonsRoutes = require("./routes/seasons");
@@ -17,10 +18,15 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  busboy({
+    highWaterMark: 5 * 1024 * 1024, // 5MB Buffer
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
 app.use("/seasons/", seasonsRoutes);
--- routes/api/season.js
+++ routes/api/season.js
@@ -1,20 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
+const fs = require("fs");
+const path = require("path");
+
 const upload = require("../../config/multer.config");
 const publisher = require("../../redis/publisher");
 
-router.post("/fileupload", upload.array("files"), (req, res) => {
-  const packet = {
-    seasonName: req.body.seasonName,
-    files: req.files.map((file) => ({
-      filename: file.filename,
-      path: file.path,
-      originalname: file.originalname,
-    })),
-  };
-  publisher.publish("message", JSON.stringify(packet));
-  res.json(packet);
+const season = require("../../models/Season.model");
+
+router.post("/createSeason", async (req, res) => {
+  const newSeason = await season.AddSeason(req.body.seasonName);
+  res.json(newSeason);
+});
+
+router.post("/fileupload/:seasonId", (req, res) => {
+  req.pipe(req.busboy);
+  req.busboy.on("file", (name, file, info) => {
+    const fileNameToWrite = `${Date.now()} - ${info.filename}`;
+    const fstream = fs.createWriteStream(path.join("uploads", fileNameToWrite));
+    file.pipe(fstream);
+    fstream.on("close", () => {
+      const packet = {
+        seasonId: req.params.seasonId,
+        fileName: fileNameToWrite,
+        originalName: info.filename
+      };
+      publisher.publish("message", JSON.stringify(packet));
+      res.json({
+        stat: info.filename,
+      });
+    });
+  });
 });
 
 module.exports = router;
