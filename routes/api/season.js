18c939954fd2f9f2b3597b0749f1d8af8a931e3c
Zishan Ahmed Onik <zisanik@gmail.com>
2023-01-19 20:24:04 -0600

    moved conversion coordination to f e

--- routes/api/season.js
+++ routes/api/season.js
@@ -4,29 +4,20 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 
-const upload = require("../../config/multer.config");
 const publisher = require("../../redis/publisher");
 
-const season = require("../../models/Season.model");
-
 router.post("/createSeason", async (req, res) => {
-  const newSeason = await season.AddSeason(req.body.seasonName);
-  res.json(newSeason);
+  publisher.publish("message", JSON.stringify(req.body));
+  res.json({ status: "ok" });
 });
 
-router.post("/fileupload/:seasonId", (req, res) => {
+router.post("/fileupload/", (req, res) => {
   req.pipe(req.busboy);
   req.busboy.on("file", (name, file, info) => {
-    const fileNameToWrite = `${Date.now()} - ${info.filename}`;
+    const fileNameToWrite = info.filename;
     const fstream = fs.createWriteStream(path.join("uploads", fileNameToWrite));
     file.pipe(fstream);
     fstream.on("close", () => {
-      const packet = {
-        seasonId: req.params.seasonId,
-        fileName: fileNameToWrite,
-        originalName: info.filename
-      };
-      publisher.publish("message", JSON.stringify(packet));
       res.json({
         stat: info.filename,
       });
